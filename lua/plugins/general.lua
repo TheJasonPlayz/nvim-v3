6e00e6f925afd2fd72b7caea6eb9bb4d4909da9f
Jason Whitman <thejasonplayz@gmail.com>
2025-05-18 19:10:46 -0600

    more stuff

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -1,25 +1,46 @@
 return {
-  { 'akinsho/toggleterm.nvim', version = "*", opts = {} },
-  {
-    'Shatur/neovim-session-manager',
-    init = function()
-      local session_manager = require('session_manager')
-      local config = require('session_manager.config')
-      vim.g.currentSessionMode = config.AutoloadMode.LastSession
+	{ 'akinsho/toggleterm.nvim', version = "*", opts = {} },
+	{
+		'Shatur/neovim-session-manager',
+		init = function()
+			local session_manager = require('session_manager')
+			local config = require('session_manager.config')
+			vim.g.currentSessionMode = config.AutoloadMode.LastSession
 
-      session_manager.setup({
-        autoload_mode = vim.g.currentSessionMode
-      })
+			session_manager.setup({
+				autoload_mode = vim.g.currentSessionMode
+			})
 
-      local toggle = function()
-        if vim.g.currentSessionMode == config.AutoloadMode.LastSession then
-          vim.g.currentSessionMode = config.AutoloadMode.Disabled
-        else
-          vim.g.currentSessionMode = config.AutoloadMode.LastSession
-        end
-        session_manager.setup({ autoload_mode = vim.g.currentSessionMode })
-      end
-      vim.api.nvim_create_user_command("ToggleSaveSession", toggle, {})
-    end
-  }
+			local toggle = function()
+				if vim.g.currentSessionMode == config.AutoloadMode.LastSession then
+					vim.g.currentSessionMode = config.AutoloadMode.Disabled
+				else
+					vim.g.currentSessionMode = config.AutoloadMode.LastSession
+				end
+				session_manager.setup({ autoload_mode = vim.g.currentSessionMode })
+			end
+			vim.api.nvim_create_user_command("SaveSessionToggle", toggle, {})
+		end
+	},
+	{
+		"nvim-pack/nvim-spectre",
+		config = function()
+			require('spectre').setup({ is_block_ui_break = true })
+
+			vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+				desc = "Toggle Spectre"
+			})
+			vim.keymap.set('n', '<leader>sw',
+				'<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+					desc = "Search current word"
+				})
+			vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+				desc = "Search current word"
+			})
+			vim.keymap.set('n', '<leader>sp',
+				'<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+					desc = "Search on current file"
+				})
+		end,
+	}
 }
